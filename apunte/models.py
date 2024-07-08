072feb976e133fc0ec6edcf75a50b6e70256b15d
tucajas <tucajas@gmail.com>
2024-07-08 07:14:14 -0300

    agreguemascaracteres en el texto

--- apunte/models.py
+++ apunte/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Apunte (models.Model):
     descripcion =   models.CharField(max_length=600)
-    texto       =   models.TextField()
+    texto       =   models.TextField(max_length=2000)
     
     def __str__(self):
         return str (self.descripcion)
