edab6f27b4d5e66eab23a34cdc4ee2b0ad67876b
tucajas <tucajas@gmail.com>
2024-07-08 13:04:12 -0300

    cors4

--- desarrolladorApp/settings.py
+++ desarrolladorApp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-i%$na8w(k8223snc_bq*d2it5rda9-9%hqloicnc7#+6l*)_)v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["https://tukapuntes.netlify.app"]
+ALLOWED_HOSTS = []
 #actualizado
 CORS_ALLOWED_ORIGINS = (
     "https://tukapuntes.netlify.app",
